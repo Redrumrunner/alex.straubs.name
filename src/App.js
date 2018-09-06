526bcee2ea45351bf251ba758614a924a9def2d8
James Straub <jamsajones@gmail.com>
2018-09-06 14:08:55 -0700

    Changed p to div because it is bad HTML to have divs inside p tags.

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <div className="App-intro">
           <Likes thing="cheese" />
           <Likes thing="beer" />
           <Likes thing="foo" />
           <LikesChild title="Title One">
             <div>One is great!</div>
           </LikesChild>
-        </p>
+        </div>
       </div>
     );
   }
